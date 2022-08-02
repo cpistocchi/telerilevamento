799d5eed9e115457f55eb44aaa3539a3c91a9725
cpistocchi <100956823+cpistocchi@users.noreply.github.com>
2022-08-02 19:32:23 +0200

    Create R_code_exam.r

---
+++ R_code_exam.r
@@ -0,0 +1,25 @@
+#ESAME TELERILEVAMENTO GEO-ECOLOGICO (A.A- 2021-2022)
+#DESERTIFICAZIONE E STAGIONALITà: IL LAGO CHAD COME CASO STUDIO
+
+#librerie:
+
+#install.packages("raster")
+
+library(raster)
+
+
+#setting della cartella di lavoro:
+setwd("C:/lab/") 
+
+
+#le immagini satellitati utilizzate sono state prodotte da LandSat 8, con risoluzione 30 m x 30 m.
+#nel caso di LandSat, le bande che saranno considerate in questa analisi:
+#banda 2: blu
+#banda 3: verde
+#banda 4: rosso
+#banda 5: NIR
+#importazione delle immagini a partire dai singoli layer:
+
+
+
+
